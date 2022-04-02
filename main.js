adca1cdfd7e82cd6d81e7bf35e76668e3f7e01e0
samandar-dev <samandarbohodirov01@gmail.com>
2022-04-02 23:41:38 +0500

    add

--- main.js
+++ main.js
@@ -12,6 +12,82 @@ const btnSpan2 = document.querySelector('.btn-span2');
 const btnSpan3 = document.querySelector('.btn-span3');
 let count = 1
 
+
+tBtn1.addEventListener('click', () => {
+  btn2.classList.add('act');
+  btnSpan2.classList.add('act-span');
+  btn3.classList.remove('act');
+  btnSpan3.classList.remove('act-span');
+
+  text.textContent = 'Choose description content';
+  tBtn1.classList.add('d-none');
+  tBtn2.classList.remove('d-none');
+  tBtn3.classList.remove('d-none');
+  tBtn4.classList.add('d-none');
+  tBtn5.classList.add('d-none');
+  count = 2
+})
+
+tBtn2.addEventListener('click', () => {
+  btn2.classList.remove('act');
+  btnSpan2.classList.remove('act-span');
+  btn3.classList.remove('act');
+  btnSpan3.classList.remove('act-span');
+
+  text.textContent = 'Choose title content';
+  tBtn1.classList.remove('d-none');
+  tBtn2.classList.add('d-none');
+  tBtn3.classList.add('d-none');
+  tBtn4.classList.add('d-none');
+  tBtn5.classList.add('d-none');
+  count = 1
+})
+
+tBtn3.addEventListener('click', () => {
+  btn2.classList.add('act');
+  btnSpan2.classList.add('act-span');
+  btn3.classList.add('act');
+  btnSpan3.classList.add('act-span');
+
+  text.textContent = 'Choose data content';
+  tBtn1.classList.add('d-none');
+  tBtn2.classList.add('d-none');
+  tBtn3.classList.add('d-none');
+  tBtn4.classList.remove('d-none');
+  tBtn5.classList.remove('d-none');
+  count = 2
+})
+
+tBtn4.addEventListener('click', () => {
+  btn2.classList.add('act');
+  btnSpan2.classList.add('act-span');
+  btn3.classList.remove('act');
+  btnSpan3.classList.remove('act-span');
+
+  text.textContent = 'Choose description content';
+  tBtn1.classList.add('d-none');
+  tBtn2.classList.remove('d-none');
+  tBtn3.classList.remove('d-none');
+  tBtn4.classList.add('d-none');
+  tBtn5.classList.add('d-none');
+  count = 2
+})
+
+tBtn5.addEventListener('click', () => {
+  btn2.classList.add('act');
+  btnSpan2.classList.add('act-span');
+  btn3.classList.add('act');
+  btnSpan3.classList.add('act-span');
+
+  text.textContent = 'Ok, were done. Thanks for sending is your data!';
+  tBtn1.classList.add('d-none');
+  tBtn2.classList.add('d-none');
+  tBtn3.classList.add('d-none');
+  tBtn4.classList.add('d-none');
+  tBtn5.classList.add('d-none');
+})
+
+
 btn1.addEventListener('click', () => {
   btn2.classList.remove('act');
   btnSpan2.classList.remove('act-span');
@@ -59,77 +135,4 @@ btn3.addEventListener('click', () => {
     tBtn5.classList.remove('d-none');
     count = 1
   }
-})
-
-tBtn1.addEventListener('click', () => {
-  btn2.classList.add('act');
-  btnSpan2.classList.add('act-span');
-  btn3.classList.remove('act');
-  btnSpan3.classList.remove('act-span');
-
-  text.textContent = 'Choose description content';
-  tBtn1.classList.add('d-none');
-  tBtn2.classList.remove('d-none');
-  tBtn3.classList.remove('d-none');
-  tBtn4.classList.add('d-none');
-  tBtn5.classList.add('d-none');
-})
-
-tBtn2.addEventListener('click', () => {
-  btn2.classList.remove('act');
-  btnSpan2.classList.remove('act-span');
-  btn3.classList.remove('act');
-  btnSpan3.classList.remove('act-span');
-
-  text.textContent = 'Choose title content';
-  tBtn1.classList.remove('d-none');
-  tBtn2.classList.add('d-none');
-  tBtn3.classList.add('d-none');
-  tBtn4.classList.add('d-none');
-  tBtn5.classList.add('d-none');
-  count = 1
-})
-
-tBtn3.addEventListener('click', () => {
-  btn2.classList.add('act');
-  btnSpan2.classList.add('act-span');
-  btn3.classList.add('act');
-  btnSpan3.classList.add('act-span');
-
-  text.textContent = 'Choose data content';
-  tBtn1.classList.add('d-none');
-  tBtn2.classList.add('d-none');
-  tBtn3.classList.add('d-none');
-  tBtn4.classList.remove('d-none');
-  tBtn5.classList.remove('d-none');
-  count = 1
-})
-
-tBtn4.addEventListener('click', () => {
-  btn2.classList.add('act');
-  btnSpan2.classList.add('act-span');
-  btn3.classList.remove('act');
-  btnSpan3.classList.remove('act-span');
-
-  text.textContent = 'Choose description content';
-  tBtn1.classList.add('d-none');
-  tBtn2.classList.remove('d-none');
-  tBtn3.classList.remove('d-none');
-  tBtn4.classList.add('d-none');
-  tBtn5.classList.add('d-none');
-  count = 1
-})
-
-tBtn5.addEventListener('click', () => {
-  btn2.classList.add('act');
-  btnSpan2.classList.add('act-span');
-  btn3.classList.add('act');
-  btnSpan3.classList.add('act-span');
-
-  text.textContent = 'Ok, were done. Thanks for sending is your data!';
-  tBtn1.classList.add('d-none');
-  tBtn2.classList.add('d-none');
-  tBtn3.classList.add('d-none');
-  tBtn4.classList.add('d-none');
-  tBtn5.classList.add('d-none');
 })
